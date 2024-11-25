a275d2128422f48e0acc281783745db6da9f5dfb
urielshira <149491002+urielshira@users.noreply.github.com>
2024-11-25 14:50:13 +0200

    ghost

--- src/Ghost.java
+++ src/Ghost.java
@@ -32,6 +32,7 @@ public class Ghost extends Entity{
         x = gp.tileSize * 7;
         y = gp.tileSize * 6;
         speed = 3;
+        direction = "";
     }
 
 
@@ -69,8 +70,8 @@ public class Ghost extends Entity{
 
     public void draw(Graphics2D g2){
 
-        g2.drawImage(blue, x, y-gp.tileSize, gp.tileSize, gp.tileSize, null);
-//        g2.drawImage(green, x-gp.tileSize, y, gp.tileSize, gp.tileSize, null);
+//        g2.drawImage(blue, x, y-gp.tileSize, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(green, x-gp.tileSize, y, gp.tileSize, gp.tileSize, null);
 //        g2.drawImage(pink, x+gp.tileSize, y, gp.tileSize, gp.tileSize, null);
 //        g2.drawImage(red, x-(gp.tileSize*2), y, gp.tileSize, gp.tileSize, null);
 //        g2.drawImage(yellow, x, y, gp.tileSize, gp.tileSize, null);
